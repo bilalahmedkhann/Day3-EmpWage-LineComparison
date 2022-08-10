8d6672fea0e1a2ebb930b5df553e993015e5d106
bilalahmedkhann <bilal.ahmed98@gmail.com>
2022-08-10 06:25:15 +0530

    [Bilal Ahmed Khan] Employee Wage

---
+++ EmployeeWage/EmployeepresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage1
+{
+    class EmployeepresentAbsent
+    {
+        public void PresentAbsent()
+        {
+            Random Empcheck = new Random();
+            int Check = Empcheck.Next(0,2);
+            int EmployeePresent = 1;
+
+            if(Check == EmployeePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            EmployeepresentAbsent employee = new EmployeepresentAbsent();
+            employee.PresentAbsent();
+        }
+    }
+}
