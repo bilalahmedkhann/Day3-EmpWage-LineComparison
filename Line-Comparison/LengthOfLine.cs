288873ec903a8f3fd5e9d7b72d3365a348fe4b17
bilalahmedkhann <bilal.ahmed98@gmail.com>
2022-08-10 06:36:32 +0530

    [Bilal Ahmed Khan] Line-Comparison Programs

---
+++ Line-Comparison/LengthOfLine.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LineComparison_Day3
+{
+    public class LengthOfLine
+    {
+        public static double LengthCalculation(int x1, int y1, int x2,int y2 )
+        {
+            double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            return lengthOfLine;
+        }
+    }
+}
---
+++ Line-Comparison/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LineComparison_Day3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckingEquality.UsingDoubleEqual();
+            CheckingEqualityOf2.UsingCompareTo();
+        }
+    }
+}
